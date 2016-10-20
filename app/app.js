4f03310d06389d6497c8758e65a14a9748414b1a
Oscar Djupfeldt <Ozsie@users.noreply.github.com>
2016-10-20 12:59:23 +0200

    Added rest factory and service

--- app/app.js
+++ app/app.js
@@ -4,4 +4,28 @@ angular.module('mashControl', [])
   $scope.increment = function() {
     $scope.variable += 1;
   }
+})
+.factory('mashControlRestFactory',function($resource) {
+  return {
+    getCurrentTemperature : function(){
+      return $resource('/temp/current/');
+    }
+  };
+})
+.factory('vesselRestService', function($q, $http, mashControlRestFactory){
+  var getCurrentTemperature = function (){
+    var deferred = $q.defer();
+    mashControlRestFactory.getCurrentTemperature().get({},
+      function(response) {
+        deferred.resolve(response.data);
+      }, function(error) {
+        console.error("Error getting current temperature");
+        deferred.reject(error);
+      });
+    return deferred.promise;
+  };
+
+  return {
+    getCurrentTemperature: getCurrentTemperature
+  };
 });
