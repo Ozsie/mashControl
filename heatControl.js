8f9b342ae7cdd38f3c07e989f70e9785faa1a868
Oscar Djupfeldt <oscdj113@gmail.com>
2016-11-14 21:18:17 +0000

    Added motor control

--- heatControl.js
+++ heatControl.js
@@ -1,4 +1,6 @@
 var fs = require('fs');
+var gpio = require("pi-gpio");
+
 var settings = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
 
 var increase = function (inputTemp, targetTemp) {
@@ -9,9 +11,58 @@ var decrease = function (inputTemp, targetTemp) {
   console.log(inputTemp + " > " + targetTemp + " decreasing heat.");
 };
 
-var turnOn = function() {};
+var turnOn = function() {
+  console.log("Turn on. Enable Pin: " + settings.motor.enablePin);
+  output(settings.motor.enablePin, 1);
+};
 
-var turnOff = function() {};
+var turnOff = function() {
+  console.log("Turn off. Enable Pin: " + settings.motor.enablePin);
+  backwards(500);
+  output(settings.motor.enablePin, 0, true);
+};
+
+var forward = function(steps) {
+  console.log("Forward " + steps + " steps");
+  for(var i = 0; i < steps; i++) {
+    setTimeout(setStep(1, 0, 1, 0), 5);
+    setTimeout(setStep(0, 1, 1, 0), 10);
+    setTimeout(setStep(0, 1, 0, 1), 15);
+    setTimeout(setStep(1, 0, 0, 1), 20);
+  }
+}
+
+var backwards = function(steps) {
+  console.log("Backward " + steps + " steps");
+  for (var i = 0; i < steps; i++) {
+    setTimeout(setStep(1, 0, 0, 1), 5);
+    setTimeout(setStep(0, 1, 0, 1), 10);
+    setTimeout(setStep(0, 1, 1, 0), 15);
+    setTimeout(setStep(1, 0, 1, 0), 20);
+  }
+};
+
+var setStep = function(w1, w2, w3, w4) {
+  output(settings.motor.coilA1Pin, w1)
+  output(settings.motor.coilA2Pin, w2)
+  output(settings.motor.coilB1Pin, w3)
+  output(settings.motor.coilB2Pin, w4)
+};
+
+var output = function(pin, value, close) {
+  gpio.open(pin, "output", function(err) {
+    gpio.write(pin, value, function() {
+      if (close && close === true) {
+        console.log("closing pin " + pin);
+        gpio.close(pin);
+      }
+    });
+  });
+};
+
+turnOn();
+forward(100);
+turnOff();
 
 module.exports = {
   increase: increase,
