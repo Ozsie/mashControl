d9d16b157e77091e25c01a6431cef4ab08b35e75
Ozsie <oscar@djupfeldt.se>
2017-02-12 21:17:03 +0100

    Removed unused require

--- test/scheduleRunner-spec.js
+++ test/scheduleRunner-spec.js
@@ -1,6 +1,5 @@
 var chai = require('chai');
 var expect = chai.expect; // we are using the "expect" style of Chai
-var gpio = require('./../gpio');
 var fs = require('fs');
 
 describe('scheduleRunner', function() {
