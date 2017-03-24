4ba3d928c9578f4ae2d4fd1f856cfcf36b3e15a9
Ozsie <oscar@djupfeldt.se>
2017-03-24 21:52:09 +0100

    Added sparge pause and boil handling

--- scheduleRunner.js
+++ scheduleRunner.js
@@ -66,7 +66,38 @@ var adjustTemperature = function(step, volume) {
   });
 };
 
-var nextStep = function(index) {
+var adjustTemperatureForBoil = function() {
+  tempSensor.readAndParse(function(err, data) {
+    if (!err) {
+      var currentTemp = data.temperature.celcius;
+      var initialDegreesToIncrease = 100 - status.initialTemp;
+      if (previousTemp === undefined) {
+        previousTemp = currentTemp;
+      }
+      var diff = parseFloat(currentTemp - previousTemp);
+      var degreesToIncrease = Math.abs(currentTemp - 100);
+      status.temperature = currentTemp;
+      status.minutes = getRunningForMinutes();
+
+      winston.info("Increase from last: " + diff + "C. Degrees left: " + degreesToIncrease + "C of " +
+      initialDegreesToIncrease + "C. " +
+      "Target: " + 100 + "C");
+
+      if (currentTemp < 100) {
+        winston.info("Under, double increase");
+        heatControl.fastIncrease();
+      } else {
+        winston.info("On mark " + currentTemp + " >= 100 holding.");
+      }
+      previousTemp = currentTemp;
+    } else {
+      winston.error(err);
+      callback(err);
+    }
+  });
+};
+
+var nextMashStep = function(index) {
   tempSensor.readAndParse(function(err, data) {
     if (!err) {
       var step = schedule.steps[index];
@@ -110,13 +141,98 @@ var nextStep = function(index) {
   });
 };
 
+var spargePause = function() {
+  tempSensor.readAndParse(function(err, data) {
+    if (!err) {
+      status.step = schedule.steps.length + 1;
+      status.stepName = "Sparge Pause";
+      status.initialTemp = data.temperature.celcius;
+      status.startTime = Date.now();
+      status.timeRemaining = schedule.spargePause * 60 * 1000;
+      winston.info("######################################################################################");
+      winston.info("#                                                                                    #");
+      winston.info("    Starting Sparge pause at " + status.startTime);
+      winston.info("    Will run for " + status.timeRemaining + " ms");
+      winston.info("    Temperature at start " + status.initialTemp + " C");
+      winston.info("    Mash water volume " + schedule.volume + " l");
+      winston.info("#                                                                                    #");
+      winston.info("######################################################################################");
+
+      var run = function() {
+        if (status.status === 'stopped') {
+          winston.info("Running step stopped");
+          return;
+        }
+        setTimeout(function() {
+          if (Date.now() - status.startTime < status.timeRemaining) {
+            run();
+          } else {
+            winston.info("## Step Sparge Pause ran for " + (Date.now() - status.startTime) + " ms. ##");
+          }
+        }, 12000);
+      };
+
+      run();
+      return status.timeRemaining;
+    } else {
+      winston.error("Could not read temperature", err);
+      throw new Error(err);
+    }
+  });
+};
+
+var boil = function() {
+  tempSensor.readAndParse(function(err, data) {
+    if (!err) {
+      status.step = schedule.steps.length + 2;
+      status.stepName = "Boil";
+      status.initialTemp = data.temperature.celcius;
+      status.startTime = Date.now();
+      status.timeRemaining = (schedule.boilTime + schedule.boilRiseTime) * 60 * 1000;
+      winston.info("######################################################################################");
+      winston.info("#                                                                                    #");
+      winston.info("    Starting Boil at " + status.startTime);
+      winston.info("    Will run for " + status.timeRemaining + " ms");
+      winston.info("    Temperature at start " + status.initialTemp + " C");
+      winston.info("    Mash water volume " + schedule.volume + " l");
+      winston.info("#                                                                                    #");
+      winston.info("######################################################################################");
+
+      var run = function() {
+        if (status.status === 'stopped') {
+          winston.info("Running step stopped");
+          return;
+        }
+        setTimeout(function() {
+          if (Date.now() - status.startTime < status.timeRemaining) {
+            adjustTemperatureForBoil();
+            run();
+          } else {
+            winston.info("## Step Boil ran for " + (Date.now() - status.startTime) + " ms. ##");
+          }
+        }, 12000);
+      };
+
+      run();
+      return status.timeRemaining;
+    } else {
+      winston.error("Could not read temperature", err);
+      throw new Error(err);
+    }
+  });
+};
+
 var calculateStepTime = function(step) {
   return (step.riseTime + step.time) * 60 * 1000;
 };
 
 var runSchedule = function(callback) {
   winston.info("Schedule: " + JSON.stringify(schedule));
-  var i = 0;
+  var mashStepIndex = 0;
+  var spargePauseRun = false;
+  var boilDone = false;
+
+  winston.info("Schedule has " + schedule.steps.length + " mash steps, Sparge pause of " + schedule.spargePause + " m and boil time" + (schedule.boilRiseTime + schedule.boilTime) + " m");
 
   var nexInMs = 0;
   var doStep = function() {
@@ -126,13 +242,26 @@ var runSchedule = function(callback) {
     }
     winston.info("Next step in " + nexInMs + " ms");
     setTimeout(function() {
-      if (i < schedule.steps.length) {
+      if (mashStepIndex < schedule.steps.length) {
         winston.info("It's time for the next step");
-        nextStep(i);
-        nexInMs = calculateStepTime(schedule.steps[i]);
+        nextMashStep(mashStepIndex);
+        nexInMs = calculateStepTime(schedule.steps[mashStepIndex]);
         doStep();
-        i++;
+        mashStepIndex++;
+      } else if (schedule.spargePause && !spargePauseRun) {
+        winston.info("It's time for the next step");
+        spargePause();
+        nexInMs = schedule.spargePause * 60 * 1000;
+        spargePauseRun = true;
+        doStep();
+      } else if (schedule.boilTime && (spargePauseRun || !schedule.spargePause) && !boilDone) {
+        winston.info("It's time for the next step");
+        boil();
+        nexInMs = (schedule.boilTime + schedule.boilRiseTime) * 60 * 1000;
+        boilDone = true
+        doStep();
       } else {
+      console.log("DONE--------------");
         callback();
       }
     }, nexInMs);
@@ -146,11 +275,14 @@ var startSchedule = function(newSchedule) {
     schedule = newSchedule;
     schedule.startTime = Date.now();
     status.status = schedule.status = 'running';
-    //heatControl.turnOn();
+    heatControl.fastDecrease();
+    heatControl.fastDecrease();
     runSchedule(function() {
       schedule.endTime = Date.now();
       winston.info("Mash done after " + (schedule.endTime - schedule.startTime) + " ms");
       status.status = schedule.status = 'done';
+      heatControl.fastDecrease();
+      heatControl.fastDecrease();
     });
     return true;
   } else {
