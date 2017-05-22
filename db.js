bac171f2f81356922ce90cc856ba757964d2030f
Ozsie <oscar@djupfeldt.se>
2017-05-22 21:16:09 +0200

    Added storage module for schedules

---
+++ db.js
@@ -0,0 +1,83 @@
+var fs = require('fs');
+var winston = require('winston');
+var uuidV4 = require('uuid/v4');
+var settings = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
+winston.add(winston.transports.File, { name:"db", filename: settings.logs.directory + '/db.log' });
+
+var store = settings.scheduleStore.directory;
+
+if (!fs.existsSync(store)){
+    fs.mkdirSync(store);
+}
+
+var schedules = {};
+
+var loadSchedules = function(callback) {
+  fs.readdir(store, (err, files) => {
+    if (err) {
+      callback(err);
+    } else {
+      files.forEach(file => {
+        var schedule = JSON.parse(fs.readFileSync(store + '/' + file, 'utf8'));
+        schedules[schedule.uuid] = schedule;
+      });
+      callback();
+    }
+  });
+};
+
+var createSchedule = function(schedule, callback) {
+  var uuid = uuidV4();
+  schedule.uuid = uuid;
+  fs.writeFile(store + '/' + uuid + '.json', JSON.stringify(schedule), 'utf8', function(err, data) {
+    if (!err) {
+      schedules[uuid] = schedule;
+    }
+    callback(err, uuid);
+  });
+};
+
+var retrieveSchedule = function(uuid, callback) {
+  if (schedules[uuid]) {
+    callback(undefined, schedules[uuid]);
+  } else {
+    winston.error("Could not find schedule with UUID " + uuid);
+    callback("Could not find schedule with UUID " + uuid);
+  }
+};
+
+var updateSchedule = function(uuid, schedule, callback) {
+  if (schedules[uuid]) {
+    fs.writeFile(store + '/' + uuid + '.json', JSON.stringify(schedule), 'utf8', function(err, data) {
+      if (!err) {
+        schedules[uuid] = schedule;
+      }
+      callback(err, uuid);
+    });
+  } else {
+    winston.error("Could not find schedule with UUID " + uuid);
+    callback("Could not find schedule with UUID " + uuid);
+  }
+};
+
+var deleteSchedule = function(uuid, callback) {
+  if (schedules[uuid]) {
+    fs.unlink(store + '/' + uuid + '.json', function(err, data) {
+      if (!err) {
+        schedules[uuid] = undefined;
+      }
+      callback(err, uuid);
+    });
+  } else {
+    winston.error("Could not find schedule with UUID " + uuid);
+    callback("Could not find schedule with UUID " + uuid);
+  }
+};
+
+module.exports = {
+  loadSchedules: loadSchedules,
+  createSchedule: createSchedule,
+  retrieveSchedule: retrieveSchedule,
+  updateSchedule: updateSchedule,
+  deleteSchedule: deleteSchedule
+};
---
+++ test/db-spec.js
@@ -0,0 +1,78 @@
+var chai = require('chai');
+var expect = chai.expect; // we are using the "expect" style of Chai
+var fs = require('fs');
+var db = require('./../db');
+
+describe('db', function() {
+
+  var uuid;
+
+  it('schedule should be stored', function(done) {
+    var schedule = JSON.parse(fs.readFileSync('test/mashControl-spec-schedule.json', 'utf8'));
+    db.createSchedule(schedule, function(err, data) {
+      expect(data).to.not.be.null;
+      uuid = data;
+      schedule.uuid = uuid;
+      var storedSchedule = JSON.parse(fs.readFileSync('schedules/' + uuid + '.json', 'utf8'));
+      expect(storedSchedule).to.deep.equal(schedule);
+      done();
+    });
+  });
+
+  it('schedule should be retrieved', function(done) {
+    db.retrieveSchedule(uuid, function(err, data) {
+      expect(data).to.not.be.null;
+      expect(data.uuid).to.equal(uuid);
+      done();
+    });
+  });
+
+  it('default schedule should not be retrieved before loading schedules', function(done) {
+    db.retrieveSchedule('default', function(err, data) {
+      expect(data).to.be.undefined;
+      done();
+    });
+  });
+
+  it('default schedule should be retrieved after loading schedules', function(done) {
+    var defaultSchedule = JSON.parse(fs.readFileSync('schedules/default.json', 'utf8'));
+    db.loadSchedules(function(err, data) {
+      if (!err) {
+        db.retrieveSchedule('default', function(err, data) {
+          expect(data).to.deep.equal(defaultSchedule);
+          done();
+        });
+      }
+    })
+  });
+
+  it('schedule should be updated', function(done) {
+    var oldSchedule = JSON.parse(fs.readFileSync('schedules/' + uuid + '.json', 'utf8'));
+    var modifiedSchedule = JSON.parse(fs.readFileSync('schedules/' + uuid + '.json', 'utf8'));
+    modifiedSchedule.boilTime = 90;
+    modifiedSchedule.name = "Test";
+    db.updateSchedule(uuid, modifiedSchedule, function(err, data) {
+      expect(data).to.not.be.null;
+      expect(data).to.equal(uuid);
+      var storedSchedule = JSON.parse(fs.readFileSync('schedules/' + uuid + '.json', 'utf8'));
+      expect(storedSchedule).to.deep.equal(modifiedSchedule);
+      expect(storedSchedule).to.not.equal(oldSchedule);
+      done();
+    });
+  });
+
+  it('schedule should be removed', function(done) {
+    var storedSchedule = JSON.parse(fs.readFileSync('schedules/' + uuid + '.json', 'utf8'));
+    db.deleteSchedule(uuid, function(err, data) {
+      expect(data).to.not.be.null;
+      expect(data).to.equal(uuid);
+      console.log("-------------" + uuid);
+      var checkFile = function() {
+        fs.accessSync('schedules/' + uuid + '.json')
+      };
+      expect(checkFile).to.throw(Error);
+      done();
+    });
+  });
+
+});
